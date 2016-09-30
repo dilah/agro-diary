56fc3d15130a1e41a77928f808439a4bd9fa5dda
dimitris <dlachanas@gmail.com>
2016-10-01 00:37:07 +0200

    wires to firebase

--- src/core/firebase/config.js
+++ src/core/firebase/config.js
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBsVVpEDrlNPEmshLcmOuE0FxhjPn0AqMg',
-  authDomain: 'todo-react-redux.firebaseapp.com',
-  databaseURL: 'https://todo-react-redux.firebaseio.com',
-  storageBucket: 'firebase-todo-react-redux.appspot.com'
+  apiKey: "AIzaSyC-Pq0pY0spwQ-EXYmYxRZy-J4oZKG9vC4",
+  authDomain: "agro-diary.firebaseapp.com",
+  databaseURL: "https://agro-diary.firebaseio.com",
+  storageBucket: "agro-diary.appspot.com",
+  messagingSenderId: "66564289086"
 };
